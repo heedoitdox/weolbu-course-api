d9332b67c2791cdd6f79905072165e26e0c1166c
youngbinary <youngbin@barogo.com>
2024-06-23 00:11:41 +0900

    database: 스키마 sql 파일 추가

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,31 @@
+DROP TABLE IF EXISTS members;
+CREATE TABLE IF NOT EXISTS members
+(
+    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+    identifier VARCHAR(100) NOT NULL,
+    password   VARCHAR(10)  NOT NULL,
+    name       VARCHAR(100) NOT NULL,
+    role       VARCHAR(100) NOT NULL,
+    UNIQUE (identifier)
+);
+
+DROP TABLE IF EXISTS courses;
+CREATE TABLE IF NOT EXISTS courses
+(
+    id          BIGINT AUTO_INCREMENT PRIMARY KEY,
+    title       VARCHAR(100) NOT NULL,
+    description VARCHAR(100) NOT NULL,
+    capacity    int,
+    price       decimal,
+    created_by  bigint
+);
+
+DROP TABLE IF EXISTS course_registraions;
+CREATE TABLE IF NOT EXISTS course_registraions
+(
+    id              BIGINT AUTO_INCREMENT PRIMARY KEY,
+    member_id       bigint NOT NULL,
+    course_id       bigint NOT NULL,
+    registration_at date,
+    UNIQUE (member_id, course_id)
+);
