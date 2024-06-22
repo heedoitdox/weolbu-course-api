23d141541c2a88406c0c7f40711ac5ee69ba7e58
youngbinary <youngbin@barogo.com>
2024-06-23 00:11:26 +0900

    config: security, webMvc config 설정 및 h2 console 설정

---
+++ src/main/java/io/heedoitdox/courseapi/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package io.heedoitdox.courseapi.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .csrf(AbstractHttpConfigurer::disable)
+        .httpBasic(AbstractHttpConfigurer::disable)
+        .sessionManagement(configurer -> configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(authorize -> authorize
+            .requestMatchers(PathRequest.toH2Console()).permitAll()
+            .anyRequest().authenticated());
+
+    return http.build();
+  }
+}
---
+++ src/main/java/io/heedoitdox/courseapi/config/WebMvcConfig.java
@@ -0,0 +1,26 @@
+package io.heedoitdox.courseapi.config;
+
+import java.nio.charset.StandardCharsets;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+  @Bean
+  @ConditionalOnProperty(name = "spring.h2.console.enabled", havingValue = "true")
+  public WebSecurityCustomizer webSecurityCustomizer() {
+    return web -> web.ignoring()
+        .requestMatchers(PathRequest.toH2Console());
+  }
+
+  @Bean
+  public StringHttpMessageConverter stringHttpMessageConverter() {
+    return new StringHttpMessageConverter(StandardCharsets.UTF_8);
+  }
+}
