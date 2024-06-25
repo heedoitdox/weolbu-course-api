c95768fad628dd864981154a4ad2c14b2161512e
youngbinary <youngbin@barogo.com>
2024-06-25 18:10:36 +0900

    config(build.gradle): text 환경에서 objectMapper 설정 추가

--- src/test/java/io/heedoitdox/courseapi/config/FixtureMonkeyUtil.java
+++ src/test/java/io/heedoitdox/courseapi/config/FixtureMonkeyUtil.java
@@ -1,12 +1,16 @@
 package io.heedoitdox.courseapi.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.navercorp.fixturemonkey.FixtureMonkey;
 import com.navercorp.fixturemonkey.api.introspector.ConstructorPropertiesArbitraryIntrospector;
 import com.navercorp.fixturemonkey.jackson.plugin.JacksonPlugin;
 
 public class FixtureMonkeyUtil {
-  private static final ObjectMapper objectMapper = new ObjectMapper();
+  private static final ObjectMapper objectMapper = new ObjectMapper()
+      .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+      .registerModule(new JavaTimeModule());
 
   private static final FixtureMonkey fixtureMonkey = FixtureMonkey.builder()
       .objectIntrospector(ConstructorPropertiesArbitraryIntrospector.INSTANCE)
