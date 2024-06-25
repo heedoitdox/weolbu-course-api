1cc1b66eb766031775c44aa464c011fbf9d459ad
youngbinary <youngbin@barogo.com>
2024-06-25 18:29:41 +0900

    feat(Course): registeredCount +1 메서드 생성

--- src/main/java/io/heedoitdox/courseapi/domain/course/Course.java
+++ src/main/java/io/heedoitdox/courseapi/domain/course/Course.java
@@ -10,6 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.Version;
 import java.math.BigDecimal;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -29,28 +30,45 @@ public class Course extends BaseTimeEntity {
   @Column(name = "id", nullable = false)
   private Long id;
 
+  @Column(nullable = false)
   private String title;
 
-  private String description;
+  @Column(nullable = false)
+  private Integer capacity;
 
-  private int capacity;
+  private Integer registeredCount;
 
+  private Double registeredRate;
+
+  @Column(nullable = false)
   private BigDecimal price;
 
   @ManyToOne
-  @JoinColumn(name = "instructor_id")
+  @JoinColumn(name = "instructor_id", nullable = false)
   private Member instructor;
 
-  public static Course create(String title, String description, int capacity, BigDecimal price) {
+  @Version
+  private Long version;
+
+  public static Course create(String title, int capacity, BigDecimal price) {
     return Course.builder()
         .title(title)
-        .description(description)
         .capacity(capacity)
+        .registeredCount(0)
+        .registeredRate(0.0)
         .price(price)
         .build();
   }
 
   public void addInstructor(Member instructor) {
     this.instructor = instructor;
+  }
+
+  public void register() {
+    if (this.registeredCount + 1 > this.capacity) {
+      throw new RuntimeException();
+    }
+
+    this.registeredCount = this.registeredCount + 1;
   }
 }
