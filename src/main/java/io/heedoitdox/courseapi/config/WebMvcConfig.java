008c7f6ac7aea02dbd53db986ad763a9be200a0b
youngbinary <youngbin@barogo.com>
2024-06-25 20:41:22 +0900

    refactor: 주석 정리

--- src/main/java/io/heedoitdox/courseapi/config/WebMvcConfig.java
+++ src/main/java/io/heedoitdox/courseapi/config/WebMvcConfig.java
@@ -2,7 +2,6 @@ package io.heedoitdox.courseapi.config;
 
 import java.nio.charset.StandardCharsets;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.StringHttpMessageConverter;
