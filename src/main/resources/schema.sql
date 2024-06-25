4d86b17d7232acad850b9bfbf559c659010c95fe
youngbinary <youngbin@barogo.com>
2024-06-25 20:41:09 +0900

    database: 스키마 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -18,10 +18,11 @@ CREATE TABLE IF NOT EXISTS courses
     id               BIGINT AUTO_INCREMENT PRIMARY KEY,
     title            VARCHAR(100)   NOT NULL,
     capacity         int            NOT NULL,
-    registered_count int default 0,
+    registered_count int            default 0,
+    registered_rate  decimal(19, 2) default 0,
     price            decimal(19, 2) NOT NULL,
     instructor_id    bigint         not null,
-    version          int default 1,
+    version          int            default 1,
     created_at       timestamp      not null,
     updated_at       timestamp
 );
@@ -34,6 +35,6 @@ CREATE TABLE IF NOT EXISTS course_registrations
     course_id       bigint    NOT NULL,
     registration_at date,
     created_at      timestamp not null,
-    updated_at      timestamp
---     UNIQUE (member_id, course_id)
+    updated_at      timestamp,
+    UNIQUE (member_id, course_id)
 );
